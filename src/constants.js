b5eb5e3bef6dfecee7b3152e82d8d840d844c532
Martin Reynolds <martin.reynolds@oneadvanced.com>
2021-10-12 03:16:26 +0000

    fix: complete restructure to fix #20

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+const State = {
+  CachePrimaryKey: 'RUSHJS_HELPER_KEY',
+  CacheMatchedKey: 'RUSHJS_HELPER_RESULT'
+}
+
+const Events = {
+  Key: 'GITHUB_EVENT_NAME',
+  Push: 'push',
+  PullRequest: 'pull_request'
+}
+
+const RefKey = 'GITHUB_REF'
+
+const CachePaths = ['common/temp']
+
+const KeyPrefix = 'rushjs-helper-'
+const RestoreKeys = [KeyPrefix]
+
+module.exports = {
+  State,
+  Events,
+  RefKey,
+  CachePaths,
+  KeyPrefix,
+  RestoreKeys
+}
---
+++ src/utils/actionUtils.js
@@ -0,0 +1,76 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const hasha = require('hasha')
+
+const { State, RefKey, KeyPrefix } = require('../constants')
+
+function isGhes () {
+  const ghUrl = new URL(process.env.GITHUB_SERVER_URL || 'https://github.com')
+  return ghUrl.hostname.toUpperCase() !== 'GITHUB.COM'
+}
+
+function isExactKeyMatch (key, cacheKey) {
+  return !!(
+    cacheKey &&
+    cacheKey.localeCompare(key, undefined, {
+      sensitivity: 'accent'
+    }) === 0
+  )
+}
+
+function setCacheState (state) {
+  core.saveState(State.CacheMatchedKey, state)
+}
+
+function getCacheState () {
+  const cacheKey = core.getState(State.CacheMatchedKey)
+  if (cacheKey) {
+    core.debug(`Cache state/key: ${cacheKey}`)
+    return cacheKey
+  }
+
+  return undefined
+}
+
+function logWarning (message) {
+  const warningPrefix = '[warning]'
+  core.info(`${warningPrefix}${message}`)
+}
+
+function isValidEvent () {
+  return RefKey in process.env && Boolean(process.env[RefKey])
+}
+
+async function runRushInstall () {
+  return exec.exec('node', ['common/scripts/install-run-rush.js', 'install'])
+}
+
+function generateCacheKey (packageManager) {
+  function getLockFile (packageManager) {
+    const packageManagers = {
+      npm: 'common/config/rush/npm-shrinkwrap.json',
+      pnpm: 'common/config/rush/pnpm-lock.yaml',
+      yarn: 'common/config/rush/yarn.lock'
+    }
+
+    const lockfile = packageManagers[packageManager]
+    core.info(`lockfile is ${lockfile}`)
+    if (!lockfile) throw new Error('Invalid package manager supplied. Valid values are `pnpm`, `npm` or `yarn`')
+    return lockfile
+  }
+
+  const lockfile = getLockFile(packageManager)
+  const lockfileHash = hasha.fromFileSync(lockfile, { algorithm: 'md5' })
+  return `${KeyPrefix}${lockfileHash}`
+}
+
+module.exports = {
+  isGhes,
+  isExactKeyMatch,
+  setCacheState,
+  getCacheState,
+  logWarning,
+  isValidEvent,
+  runRushInstall,
+  generateCacheKey
+}
